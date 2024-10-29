a74bd0f9f98fe44cea8a03614a157542504a875b
florian <f.code1000@gmail.com>
2024-10-29 13:57:16 +0100

    aufgabe1 aber in müll

--- main.cpp
+++ main.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    if(argc <= 1) {
+        cout << "No file provided." << endl;
+        return 0;
+    }
+    ostream* output = &cout;
+    ofstream output_file;
+    if(argc == 3) {
+        output_file.open (argv[2]);
+        output = &output_file;
+    }
+
+    string line;
+    ifstream source (argv[1]);
+    if (source.is_open())
+    {
+        bool last_new_line = false;
+        while ( getline (source,line) )
+        {
+            for (int i = 0; i < line.length(); i++) {
+                if(!isalpha(line[i])) {
+                    if (!last_new_line) {
+                        *output <<  '\n';
+                        last_new_line = true;
+                    }
+                }
+                else {
+                    *output << line[i];
+                    last_new_line = false;
+                }
+            }
+            if (!last_new_line) {
+                *output <<  '\n';
+                last_new_line = true;
+            }
+
+        }
+        source.close();
+    }
     return 0;
 }
